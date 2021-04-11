b79ce16f03fd82b26f41b69880a497932e4060e3
TessMchd <tessmchd@hotmail.fr>
2021-04-11 18:30:24 +0200

    Update GameController.php

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -111,7 +111,7 @@ class GameController extends AbstractController
                 $set = new Round();
                 $set->setGame($game);
                 $set->setCreated(new \DateTime('now'));
-                $set->setSetNumber(1);
+                $set->setSetNumber($game->getSets()[count($game->getSets())-1]->getSetNumber()+1);
 
                 $cards = $cardRepository->findAll();
                 $tCards = [];
